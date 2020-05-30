ba86c594575191993b875ea62fe22757b036ec0f
Cespresso <Cespresso@users.noreply.github.com>
2020-05-30 06:32:59 +0000

    deploy: 63cf38bd13bde83eeb29b7e8634f745889d24739

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,6 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "3da03cee5f98f6962ab1dab1eed186e1",
+"apple-app-site-association": "8cd5f5f0de50978f98838a271bf6f61c",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/LICENSE": "34da31f697be5f2fcfacf877df9adb0a",
